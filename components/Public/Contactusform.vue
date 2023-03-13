a506ec2f51d61b6cdc75332d4e7f5bba1abc3afd
vivekrawat <vivekrawat7534@gmail.com>
2023-03-13 21:09:45 +0530

    fix/contact-us-designs

--- components/Public/Contactusform.vue
+++ components/Public/Contactusform.vue
@@ -2,23 +2,23 @@
   <div class="pt-[100px]">
     <div class="flex flex-col justify-center items-center">
       <div
-        class="text-center md:text-left text-white font-bold text-[34px] px-3"
+        class="text-center md:text-left dark:text-white font-bold text-[34px] px-3"
       >
         Fill out this form for booking a<br />
         consultant advising session.
       </div>
 
       <form @submit.prevent="submitform" class="w-full lg:w-7/12 mt-20">
-        <div class="flex justify-between items-center w-12/12 mb-2">
+        <div class="flex justify-between items-center w-12/12 mb-2 dark:text-white text-[#696969] ">
           <input
-            class="w-6/12 mr-2"
+            class="w-6/12 mr-2 dark:bg-[#222] border-none focus:ring-primary bg-[#f5f5f5] placeholder-[#696969]"
             type="text"
             placeholder="Your Name"
             id=""
             v-model="registration.name"
           />
           <input
-            class="w-6/12 ml-2"
+            class="w-6/12 ml-2 dark:bg-[#222] border-none focus:ring-primary bg-[#f5f5f5] placeholder-[#696969]"
             type="email"
             placeholder="Email"
             id=""
@@ -27,21 +27,21 @@
         </div>
 
         <input
-          class="w-full my-2"
+          class="w-full my-2 dark:bg-[#222] border-none focus:ring-primary bg-[#f5f5f5] placeholder-[#696969]"
           type="text"
           placeholder="Subject"
           id=""
           v-model="registration.subject"
         />
         <textarea
-          class="w-full text-white my-2 min-h-[220px]"
+          class="w-full dark:text-white my-2 min-h-[220px] dark:bg-[#222] border-none focus:ring-primary bg-[#f5f5f5] placeholder-[#696969]"
           placeholder="Your Mesage"
           v-model="registration.message"
         ></textarea>
 
         <div class="w-full flex justify-center">
           <button
-            class="text-[14px] text-white font-bold px-12 py-4 bg-[#20ad96] rounded-md mt-12"
+            class="text-[14px] text-white font-bold px-12 py-4 bg-[#20ad96] rounded-md mt-8"
           >
             Submit
           </button>
@@ -84,14 +84,9 @@ methods: {
 input,
 textarea {
   padding: 10px 20px;
-  background-color: #222;
-  border: solid 0.5px #333;
   border-radius: 3px;
   outline: none;
 }
 
-input::placeholder,
-textarea::placeholder {
-  color: #696969;
-}
+
 </style>
--- pages/frontend-template/About/Contactus.vue
+++ pages/frontend-template/About/Contactus.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="blue py-[100px] px-5 sm:px-[100px]">
-    <div class="">
+  <div class="dark:blue py-[100px] font-body px-5 ">
+
+    <div class="container">
       <div class="flex flex-col">
         <div class="mb-[40px]">
-          <h2 class="title text-center">
+          <h2 class="title text-center dark:text-white">
             For more information about our courses, get in <br />
             touch with MaxCoach via contacts
           </h2>
         </div>
 
         <div
-          class="flex flex-wrap md:flex-row items-start justify-between md:items-center mt-10"
+          class="flex flex-wrap md:flex-row items-start justify-between mt-10"
         >
           <contactdetails />
         </div>
       </div>
 
-      <div class="md:ml-20">
-        <div class="w-12/12 h-[85vh] pb-10 md:pb-0">
+      <div class="sm:p-2 mt-10">
+        <div class="w-full h-[50vh] pb-10 md:pb-0">
           <iframe
-            class="w-full h-full"
+            class="w-full h-full z-10 inline-block"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2136.986005919501!2d-73.9685579655238!3d40.75862446708152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258e4a1c884e5%3A0x24fe1071086b36d5!2sThe%20Atrium!5e0!3m2!1sen!2sbd!4v1585132512970!5m2!1sen!2sbd"
           ></iframe>
         </div>
@@ -53,6 +54,5 @@ definePageMeta({
   line-height: 1;
   font-weight: 700;
   margin-bottom: 0;
-  color: white;
 }
 </style>
